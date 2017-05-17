9f979c9d9d28718b745a3f58627d45b66d94d7d6
zhengheng <1020423231@qq.com>
2017-05-17 16:05:40 +0800

    Initial commit

---
+++ app/src/main/java/zixue/com/myrecyclerviewtest/MainActivity.java
@@ -0,0 +1,20 @@
+package zixue.com.myrecyclerviewtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+       getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
